619937b61a483219378a4728debff184963a36e2
Alexey Yaremenko <yaremenko@outlook.com>
2020-05-15 17:00:51 +0300

    bootstrapper implementation

---
+++ Editor/Bootstrapper.cs
@@ -0,0 +1,59 @@
+﻿using UnityEditor;
+
+namespace OneSignalPush.Bootstrapper
+{
+    [InitializeOnLoad]
+    static class Bootstrapper
+    {
+        static Bootstrapper()
+        {
+            Manifest manifest = new Manifest();
+            manifest.Fetch();
+
+            bool manifestUpdated = false;
+
+            if (!manifest.IsRegistryExists(ScopeRegistriesConfig.NpmjsScopeRegistryUrl))
+            {
+                manifest.AddScopeRegistry(ScopeRegistriesConfig.NpmjsScopeRegistry);
+                manifestUpdated = true;
+            }
+            else
+            {
+                var npmjsScopeRegistry = manifest.GetScopeRegistry(ScopeRegistriesConfig.NpmjsScopeRegistryUrl);
+                if (!npmjsScopeRegistry.HasScope(ScopeRegistriesConfig.OneSignalScope))
+                {
+                    npmjsScopeRegistry.AddScope(ScopeRegistriesConfig.OneSignalScope);
+                    manifestUpdated = true;
+                }
+            }
+
+            // UnityEditor.PackageManager.Client.Add(ScopeRegistriesConfig.OneSignalCoreName);
+            // UnityEditor.PackageManager.Client.Add method doesn't work in Unity versions older then 2019.
+            // Thus, we need to manually add dependencies.
+            // Probably we need to use OneSignalUpdateRequest to get the latest package version
+
+            if (!manifest.IsDependencyExists(ScopeRegistriesConfig.OneSignalCoreName))
+            {
+                manifest.AddDependency(ScopeRegistriesConfig.OneSignalCoreName, ScopeRegistriesConfig.OneSignalCoreVersion);
+                manifestUpdated = true;
+            }
+
+            if (!manifest.IsDependencyExists(ScopeRegistriesConfig.OneSignalAndroidName))
+            {
+                manifest.AddDependency(ScopeRegistriesConfig.OneSignalAndroidName, ScopeRegistriesConfig.OneSignalAndroidVersion);
+                manifestUpdated = true;
+            }
+
+            if (!manifest.IsDependencyExists(ScopeRegistriesConfig.OneSignaliOSName))
+            {
+                manifest.AddDependency(ScopeRegistriesConfig.OneSignaliOSName, ScopeRegistriesConfig.OneSignalIosVersion);
+                manifestUpdated = true;
+            }
+
+            if (manifestUpdated)
+            {
+                manifest.ApplyChanges();
+            }
+        }
+    }
+}
---
+++ Editor/ScopeRegistriesConfig.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace OneSignalPush.Bootstrapper
+{
+    static class ScopeRegistriesConfig
+    {
+        public static readonly string OneSignalScope = "com.onesignal";
+
+        public static readonly string OneSignalCoreName = "com.onesignal.unity.core";
+        public static readonly string OneSignalCoreVersion = "0.0.1";
+
+        public static readonly string OneSignaliOSName = "com.onesignal.unity.ios";
+        public static readonly string OneSignalIosVersion = "0.0.1";
+
+        public static readonly string OneSignalAndroidName = "com.onesignal.unity.android";
+        public static readonly string OneSignalAndroidVersion = "0.0.1";
+
+        public static readonly string GoogleScopeRegistryUrl = "https://unityregistry-pa.googleapis.com";
+        public static readonly string NpmjsScopeRegistryUrl = "https://registry.npmjs.org/";
+
+        public static ScopeRegistry GoogleScopeRegistry =>
+            new ScopeRegistry("Game Package Registry by Google",
+                GoogleScopeRegistryUrl,
+                new HashSet<string>
+                {
+                    "com.google"
+                });
+
+        public static ScopeRegistry NpmjsScopeRegistry =>
+            new ScopeRegistry("npmjs",
+                NpmjsScopeRegistryUrl,
+                new HashSet<string>
+                {
+                    "com.stansassets",
+                    OneSignalScope
+                });
+    }
+}
