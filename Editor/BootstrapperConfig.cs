8f07ec6dcc41948653573d463f41646796bc1900
Stanislav Osipov <lacost.st@gmail.com>
2021-03-25 01:08:47 +0200

    chore: config fix

--- Editor/BootstrapperConfig.cs
+++ Editor/BootstrapperConfig.cs
@@ -4,18 +4,23 @@ namespace Com.OneSignal.Bootstrapper
 {
     static class BootstrapperConfig
     {
-        public static readonly string OneSignalScope = "com.onesignal";
-        public static readonly string BootstrapperPackageName = $"{OneSignalScope}.unity.bootstrap";
+        public static readonly string OneSignalScope = "com.onesignal.unity";
+        public static readonly string BootstrapperPackageName = $"{OneSignalScope}.bootstrap";
 
-        public static readonly string OneSignalCoreName = $"{OneSignalScope}.unity.core";
-        public static readonly string OneSignalIOSName = $"{OneSignalScope}.unity.ios";
-        public static readonly string OneSignalAndroidName = $"{OneSignalScope}.unity.android";
+        public static readonly string OneSignalCoreName = $"{OneSignalScope}.core";
+        public static readonly string OneSignalIOSName = $"{OneSignalScope}.ios";
+        public static readonly string OneSignalAndroidName = $"{OneSignalScope}.android";
 
         public static readonly string GoogleScopeRegistryUrl = "https://unityregistry-pa.googleapis.com";
         public static readonly string NpmjsScopeRegistryUrl = "https://registry.npmjs.org/";
 
-        public const string GitHubRepositoryURL = @"ssh://git@github.com:StansAssets/OneSignal-Unity-SDK.git";
-        public const string BootstrapperFolderPath = @"Assets/OneSignalBootstrap/Editor";
+        public const string GitHubRepositoryURL = @"ssh://git@github.com:OneSignal/OneSignal-Unity-SDK.git";
+        public const string BootstrapperFolderPath = @"Assets/OneSignalBootstrap";
+
+        public static readonly string[] OutdatedSDKDirectories = {
+            @"Assets/OneSignal",
+            @"Assets/Plugins/Android/OneSignalConfig.plugin",
+        };
 
         public static ScopeRegistry GoogleScopeRegistry =>
             new ScopeRegistry("Game Package Registry by Google",
